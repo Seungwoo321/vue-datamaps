68e1059bd94c611e14d76852d1f96f3072476e9b
swlee <swlee@mz.co.kr>
2019-11-19 13:53:44 +0900

    Update arcs, graticules, zoom

--- demo/App.vue
+++ demo/App.vue
@@ -63,10 +63,42 @@
     </div>
     <hr>
     <div>
+        <h4>Projections & Graticules:</h4>
+        <vue-datamaps
+            :scope="graticules.scope"
+            :projection="graticules.projection"
+            :projectionConfig="graticules.projectionConfig"
+            :fills="graticules.fills"
+            :data="graticules.data"
+            :arcConfig="graticules.arcConfig"
+            arc
+        >
+        </vue-datamaps>
+    </div>
+    <hr>
+    <div>
+        <h4>Zoom:</h4>
+        <vue-datamaps
+            :scope="zoom.scope"
+            :data="zoom.data"
+            :fills="zoom.fills"
+            :bubblesConfig="zoom.bubblesConfig"
+            bubbles
+            @custom:popup="zoomBubblePopupTemplate"
+            popupTemplate
+            :setProjection="setProjection('zoom')"
+        >
+            <div slot="hoverinfo" class="hoverinfo" style="text-align:center;">
+                {{ zoom.popupData.name }}
+            </div>
+        </vue-datamaps>
+    </div>
+    <hr>
+    <div>
         <h4>Korea:</h4>
         <vue-datamaps
             :scope="korea.scope"
-            :setProjection="setProjection"
+            :setProjection="setProjection('korea')"
             :fills="korea.fills"
             :data="korea.data"
         />
@@ -470,6 +502,93 @@ export default {
                     strokeWidth: 1,
                     arcSharpness: 1.4
                 }
+            },
+            graticules: {
+                scope: 'world',
+                projection: 'Orthographic',
+                fills: {
+                    defaultFill: '#ABDDA4',
+                    gt50: d3.schemeCategory20[Math.floor(Math.random() * 20)],
+                    eq50: d3.schemeCategory20b[Math.floor(Math.random() * 20)],
+                    lt25: d3.schemeCategory20c[Math.floor(Math.random() * 20)],
+                    gt75: d3.schemeCategory20[Math.floor(Math.random() * 20)],
+                    lt50: d3.schemeCategory10[Math.floor(Math.random() * 10)],
+                    eq0: d3.schemeCategory10[Math.floor(Math.random() * 1)],
+                    pink: '#0fa0fa',
+                    gt500: d3.schemeCategory10[Math.floor(Math.random() * 1)]
+                },
+                data: {
+                    'USA': { fillKey: 'lt50' },
+                    'MEX': { fillKey: 'lt25' },
+                    'CAN': { fillKey: 'gt50' },
+                    'GTM': { fillKey: 'gt500' },
+                    'HND': { fillKey: 'eq50' },
+                    'BLZ': { fillKey: 'pink' },
+                    'GRL': { fillKey: 'eq0' }
+                },
+                projectionConfig: {
+                    rotation: [97, -30]
+                },
+                arcConfig: {
+                    data: [
+                        {
+                            origin: {
+                                latitude: 61,
+                                longitude: -149
+                            },
+                            destination: {
+                                latitude: -22,
+                                longitude: -43
+                            }
+                        }
+                    ],
+                    greatArc: true,
+                    animationSpeed: 2000
+                }
+            },
+            zoom: {
+                scope: 'world',
+                fills: {
+                    defaultFill: '#ABDDA4',
+                    gt50: d3.schemeCategory20[Math.floor(Math.random() * 20)],
+                    eq50: d3.schemeCategory20b[Math.floor(Math.random() * 20)],
+                    lt25: d3.schemeCategory20c[Math.floor(Math.random() * 20)],
+                    gt75: d3.schemeCategory20[Math.floor(Math.random() * 20)],
+                    lt50: d3.schemeCategory10[Math.floor(Math.random() * 10)],
+                    eq0: d3.schemeCategory10[Math.floor(Math.random() * 1)],
+                    pink: '#0fa0fa',
+                    gt500: d3.schemeCategory10[Math.floor(Math.random() * 1)]
+                },
+                data: {
+                    'ZAF': { fillKey: 'gt50' },
+                    'ZWE': { fillKey: 'lt25' },
+                    'NGA': { fillKey: 'lt50' },
+                    'MOZ': { fillKey: 'eq50' },
+                    'MDG': { fillKey: 'eq50' },
+                    'EGY': { fillKey: 'gt75' },
+                    'TZA': { fillKey: 'gt75' },
+                    'LBY': { fillKey: 'eq0' },
+                    'DZA': { fillKey: 'gt500' },
+                    'SSD': { fillKey: 'pink' },
+                    'SOM': { fillKey: 'gt50' },
+                    'GIB': { fillKey: 'eq50' },
+                    'AGO': { fillKey: 'lt50' }
+                },
+                bubblesConfig: {
+                    data: [
+                        { name: 'Bubble 1', latitude: 21.32, longitude: -7.32, radius: 45, fillKey: 'gt500' },
+                        { name: 'Bubble 2', latitude: 12.32, longitude: 27.32, radius: 25, fillKey: 'eq0' },
+                        { name: 'Bubble 3', latitude: 0.32, longitude: 23.32, radius: 35, fillKey: 'lt25' },
+                        { name: 'Bubble 4', latitude: -31.32, longitude: 23.32, radius: 55, fillKey: 'eq50' }
+                    ]
+                },
+                geographyConfig: {
+                    popupOnHover: false,
+                    highlightOnHover: false
+                },
+                popupData: {
+                    name: ''
+                }
             }
         }
     },
@@ -479,13 +598,28 @@ export default {
         }
     },
     methods: {
-        setProjection (d3, element) {
-            const projection = d3.geoMercator().center([0, 0])
-                .rotate([-128, -36])
-                .scale(5000)
-                .translate([element.offsetWidth / 2, element.offsetHeight / 2])
-            const path = d3.geoPath().projection(projection)
-            return { projection, path }
+        setProjection (type) {
+            const createProjection = {
+                korea: function (d3, element) {
+                    const projection = d3.geoMercator().center([0, 0])
+                        .rotate([-128, -36])
+                        .scale(5000)
+                        .translate([element.offsetWidth / 2, element.offsetHeight / 2])
+                    const path = d3.geoPath().projection(projection)
+                    return { projection, path }
+                },
+                zoom: function (d3, element) {
+                    var projection = d3.geoEquirectangular()
+                        .center([23, -3])
+                        .rotate([4.4, 0])
+                        .scale(400)
+                        .translate([element.offsetWidth / 2, element.offsetHeight / 2])
+                    var path = d3.geoPath()
+                        .projection(projection)
+                    return { path: path, projection: projection }
+                }
+            }
+            return createProjection[type]
         },
         popupTemplate ({ geography, data }) {
             this.stateLabels.popupData = `${geography.properties.name}\nElectoral Votes: ${data.electoralVotes}`
@@ -496,6 +630,9 @@ export default {
                 date: data.date,
                 country: data.country
             }
+        },
+        zoomBubblePopupTemplate ({ geography, data }) {
+            this.zoom.popupData.name = data ? `Bubble for ${data.name}` : geography.properties.name
         }
     },
     mounted () {
